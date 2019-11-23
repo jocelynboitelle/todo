6df14992bc404c9962f6333239395d72b4aa303b
jocelyn <jboitelle@botdesign.net>
2019-11-23 13:18:11 +0100

    css

--- src/services/inMemHero.service.ts
+++ src/services/inMemHero.service.ts
@@ -1,13 +1,38 @@
-import { InMemoryDbService } from 'angular-in-memory-web-api';
-import { Todo } from '../models/todo.model';
+import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Todo } from "../models/todo.model";
 
 export class InMemTodoService implements InMemoryDbService {
   createDb() {
     let todos: Todo[] = [
-      { id: 1, title: 'Windstorm', done: true },
-      { id: 2, title: 'Bombasto', done: true },
-      { id: 3, title: 'Magneta', done: false },
-      { id: 4, title: 'Tornado', done: true }
+      { id: 1, title: "make the boxes", description: "", done: true },
+      { id: 2, title: "relocation", description: "", done: false },
+      { id: 3, title: "overkill todo", description: "", done: true },
+      { id: 4, title: "maternity appointment", description: "", done: true },
+      {
+        id: 5,
+        title: "find a maternal assistant",
+        description: "",
+        done: false
+      },
+      {
+        id: 6,
+        title: "have a job",
+        description: "have a salary of 28K",
+        done: false
+      },
+      {
+        id: 7,
+        title: "wash the dishes",
+        description: "",
+        done: true
+      },
+      {
+        id: 8,
+        title: "administrative papers",
+        description:
+          "-SFR\n -EDF\n -Gas\n -Water\n -Maternity\n -Taxes\n -Location",
+        done: false
+      }
     ];
     return { todos };
   }
