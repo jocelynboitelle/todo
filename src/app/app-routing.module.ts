091f1eed996119b540e4ccffe00d8cdb3c792fdf
jocelyn <jboitelle@botdesign.net>
2019-12-06 15:43:04 +0100

    little css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { CreateTodoComponent } from './create-todo/create-todo.component';
-import { ReadTodoComponent } from './read-todo/read-todo.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { CreateTodoComponent } from "./create-todo/create-todo.component";
+import { ReadTodoComponent } from "./read-todo/read-todo.component";
+import { TodoListComponent } from "./todo-list/todo-list.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'todos', pathMatch: 'full' },
-  { path: 'todos', component: TodoListComponent },
-  { path: 'todos/create', component: CreateTodoComponent },
-  { path: 'todos/:id', component: ReadTodoComponent },
-  { path: '**', redirectTo: 'todos' }
+  { path: "", redirectTo: "todos", pathMatch: "full" },
+  {
+    path: "todos",
+    children: [
+      { path: "create", component: CreateTodoComponent },
+      { path: ":id", component: ReadTodoComponent },
+      { path: "", component: TodoListComponent }
+    ]
+  },
+  { path: "**", redirectTo: "todos" }
 ];
 
 @NgModule({
